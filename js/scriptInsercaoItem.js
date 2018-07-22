a3e7ad0626526ef19157602097dfa8f7fc355bda
Aline Cristina <ninanerd15@gmail.com>
2018-07-22 19:58:28 -0300

    front end completo

--- js/scriptInsercaoItem.js
+++ js/scriptInsercaoItem.js
@@ -25,6 +25,8 @@ function insereItem(item) {
   let liDesc = document.createElement('li');
   let liTags = document.createElement('li');
   let liData = document.createElement('li');
+  let botaoOp = document.createElement('button');
+  let iBotao = document.createElement('i');
 
   ulDesc.setAttribute('class', 'submenuItems');
   liDesc.setAttribute('id', 'subItem');
@@ -33,9 +35,15 @@ function insereItem(item) {
   liData.setAttribute('class', 'liDescricao');
   liData.setAttribute('style', 'text-align: right');
   divDropDown.setAttribute('class', 'dropdownlink');
+  botaoOp.setAttribute('class', 'opcoesItem');
+  botaoOp.setAttribute('data-toggle', 'modal');
+  iBotao.setAttribute('class', 'fa fa-ellipsis-v');
+
+  botaoOp.appendChild(iBotao);
 
   if(item.tipo == 'tarefa'){
     liNome.setAttribute('id', 'liTarefa');
+    botaoOp.setAttribute('data-target', '#btaoOpcaoModalTarefa');
 
     let label = document.createElement('label');
     let input = document.createElement('input');
@@ -53,15 +61,22 @@ function insereItem(item) {
     label.appendChild(input);
     label.appendChild(span);
     liNome.appendChild(label);
+    liNome.appendChild(botaoOp);
     liNome.appendChild(divDropDown);
 
   } else if(item.tipo == 'lembrete'){
+    botaoOp.setAttribute('data-target', '#btaoOpcaoModalLembrete');
+
     liNome.setAttribute('id', 'liLembrete');
     divDropDown.innerHTML = item.nome;
+    liNome.appendChild(botaoOp);
     liNome.appendChild(divDropDown);
   } else {
+    botaoOp.setAttribute('data-target', '#btaoOpcaoModalSimples');
+
     liNome.setAttribute('id', 'liSimples');
     divDropDown.innerHTML = item.nome;
+    liNome.appendChild(botaoOp);
     liNome.appendChild(divDropDown);
   }
 
