7f69b5b69d033dfd8873c079ab28c743b85c9030
Pedro Lucas Silva <pedrolsilva00@gmail.com>
2018-07-28 06:35:31 -0300

    Algumas alterações e adição do controller de tag

---
+++ Organizer/src/java/br/cefetmg/inf/organizer/controller/CreateTag.java
@@ -0,0 +1,41 @@
+package br.cefetmg.inf.organizer.controller;
+
+import br.cefetmg.inf.organizer.model.domain.Tag;
+import br.cefetmg.inf.organizer.model.domain.User;
+import br.cefetmg.inf.organizer.model.service.IKeepTag;
+import br.cefetmg.inf.organizer.model.service.impl.KeepTag;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class CreateTag implements GenericProcess {
+
+    @Override
+    public String execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+
+        String pageJSP = "";
+
+        HttpSession session = req.getSession();
+        User user = (User) session.getAttribute("user");
+
+        String nameTag = req.getParameter("");// trocar quando colocar o jsp
+
+        Tag tag = new Tag();
+
+        tag.setTagName(nameTag);
+        tag.setUser(user);
+        tag.setSeqTag(null);
+
+        IKeepTag keepTag = new KeepTag();
+        boolean result = keepTag.createTag(tag);
+
+        if (result == false) {
+            //exception 
+        } else {
+            keepTag.updateTagId(tag, keepTag.searchTagByName(nameTag, user));
+            pageJSP = "/index.jsp";// !!!!
+        }
+        return pageJSP;
+    }
+
+}
